15c68893068ce6b338845e4f5ef4044411d810a2
aajengui <anis.ajengui@tessi.fr>
2025-05-30 16:46:47 +0200

    Add DependencyInjection to register aa_performance_analyzer parameter

--- src/DependencyInjection/PerformanceAnalyzerExtension.php
+++ src/DependencyInjection/PerformanceAnalyzerExtension.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace AA\PerformanceAnalyzer\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class PerformanceAnalyzerExtension extends Extension
@@ -16,9 +16,14 @@ class PerformanceAnalyzerExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('aa_performance_analyzer', $config);
+
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
+    }
 
-        $container->setParameter('aa_performance_analyzer.config', $config);
+    public function getAlias(): string
+    {
+        return 'aa_performance_analyzer';
     }
 }
